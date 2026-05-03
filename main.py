8bd7e5e7b9c396173899c258ff97cdc1a922f632
hazzard083 <valdeniogomesdesajunior@gmail.com>
2026-05-03 18:35:09 -0300

    ✨ feat(main): implementa funcionalidade de atualização de animal

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ while True:
 					tipo = "Caprino"
 				elif tipo == 3:
 					tipo = "Ovino"
-				elif tipo == 3:
+				elif tipo == 4:
 					tipo = "Suíno"
 
 				if status == 1:
@@ -134,10 +134,109 @@ while True:
 				else:
 					print("Animal não encontrado.")
 
-				nova_busca = input("Deseja realizar uma nova busca? (s/n): ")
+				nova_busca = input("Deseja realizar uma nova busca? (s/n): ").lower()
 
 				if nova_busca != "s":
 					break
+		elif op == 3:
+			print("=" * 40)
+			print("          ATUALIZAR ANIMAL          ")
+			print("=" * 40)
+
+			while True:
+				while True:
+					print("Qual informação deseja atualizar?")
+					print("1 - Tipo")
+					print("2 - Identificação")
+					print("3 - Status")
+
+					op = int(input("Escolha (1-3): "))
+
+					if op in (1, 2, 3):
+						break
+					else:
+						print("Opção inválida. Tente novamente.")
+
+				if op == 1:
+					indentificacao = input("Identificação (brinco ou número único): ")
+
+					for animal in animais:
+						if animal[1] == indentificacao:
+							while True:
+								print("Tipo de Animal: ")
+								print("1 - Bovino de Leite")
+								print("2 - Caprino")
+								print("3 - Ovino")
+								print("4 - Suíno")
+
+								tipo = int(input("Escolha (1-4): "))
+
+								if tipo in (1, 2, 3, 4):
+									break
+								else:
+									print("Opção inválida. Tente novamente.")
+
+							if tipo == 1:
+								tipo = "Bovino de Leite"
+							elif tipo == 2:
+								tipo = "Caprino"
+							elif tipo == 3:
+								tipo = "Ovino"
+							elif tipo == 4:
+								tipo = "Suíno"
+
+							animal[0] = tipo
+							print(f"Animal ({animal[1]}) atualizado com sucesso!")
+							break
+					else:
+						print("Animal não encontrado.")
+				elif op == 2:
+					indentificacao = input("Identificação (brinco ou número único): ")
+
+					for animal in animais:
+						if animal[1] == indentificacao:
+							nova_identificacao = input("Nova identificação (brinco ou número único): ")
+
+							animal[1] = nova_identificacao
+							print(f"Animal ({animal[1]}) atualizado com sucesso!")
+							break
+					else:
+						print("Animal não encontrado.")
+				elif op == 3:
+					indentificacao = input("Identificação (brinco ou número único): ")
+
+					for animal in animais:
+						if animal[1] == indentificacao:
+							while True:
+								print("Status do Animal: ")
+								print("1 - Em lactação")
+								print("2 - Para engorda")
+								print("3 - Disponível p/ venda")
+
+								status = int(input("Escolha (1-3): "))
+
+								if status in (1, 2, 3):
+									break
+								else:
+									print("Opção inválida. Tente novamente.")
+
+							if status == 1:
+								status = "Em lactação"
+							elif status == 2:
+								status = "Para engorda"
+							elif status == 3:
+								status = "Disponível p/ venda"
+
+							animal[2] = status
+							print(f"Animal ({animal[1]}) atualizado com sucesso!")
+							break
+
+				nova_alteracao = input("Deseja realizar uma nova alteração? (s/n): ").lower()
+
+				if nova_alteracao != "s":
+					break
+		elif op == 4:
+			print(4)						
 	else:
 		print("=" * 40)
 		print("          MENU DO CLIENTE          ")
