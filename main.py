22cdc1bb8414f770678aee0b053af0d41bc82920
hazzard083 <valdeniogomesdesajunior@gmail.com>
2026-05-03 16:59:37 -0300

    ✨ feat(main): adiciona menus para administrador e cliente após login

--- main.py
+++ main.py
@@ -19,3 +19,49 @@ while not logado:
 			break
 	else:
 		print("As credenciais informadas não existem.")
+
+while True:
+	if admin:
+		print("=" * 40)
+		print("          MENU DO ADMINISTRADOR          ")
+		print("=" * 40)
+
+		print("")
+
+		print("1 - Cadastrar Animal")
+		print("2 - Buscar Animal")
+		print("3 - Atualizar Animal")
+		print("4 - Remover Animal")
+		print("5 - Listar Animal")
+
+		print("")
+
+		print("6 - Registrar Produção de Leite")
+		print("7 - Adicionar Produto ao Estoque")
+		print("8 - Ver Estoque Atual")
+
+		print("")
+
+		print("9 - Extra")
+
+		print("")
+
+		op = int(input("Escolha uma opção: "))
+
+	else:
+		print("=" * 40)
+		print("          MENU DO CLIENTE          ")
+		print("=" * 40)
+
+		print("")
+
+		print("1 - Ver Estoque Disponível")
+		print("2 - Comprar Produto")
+		print("3 - Comprar Animal")
+		print("4 - Agendar Retirada")
+		print("5 - Registrar Interesse em Produto")
+		print("6 - Ver Meus Interesses")
+
+		print("")
+
+		op = int(input("Escolha uma opção: "))
