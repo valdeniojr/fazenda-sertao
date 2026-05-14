813195851ec767b85f1363665e141129bde84e5c
hazzard083 <valdeniogomesdesajunior@gmail.com>
2026-05-14 19:33:10 -0300

    ♻️ refactor(main): simplifica atribuição de 'admin' removendo verificação redundante

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ while not logado:
 		if user[0] == usuario and user[1] == senha:
 			logado = True
 			print("Login realizado com sucesso!")
-			if user[2]:
-				admin = user[2]
+			admin = user[2]
 			break
 	else:
 		print("As credenciais informadas não existem.")
