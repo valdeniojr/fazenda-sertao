f1a2ea1b848f2e844604ce43c2f3a5a059a3b854
hazzard083 <valdeniogomesdesajunior@gmail.com>
2026-06-08 10:29:50 -0300

    ✨ feat(nota_fiscal): adiciona função para gerar nota fiscal em PDF

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from fpdf import FPDF
+from datetime import datetime
 
 def clearCMD():
   os.system('cls' if os.name == 'nt' else 'clear')
@@ -36,3 +38,61 @@ def gerar_id(lista_tipos, lista, tipo):
             return confirm
 
         i += 1
+    
+def gerar_nota_fiscal(cliente, endereco, nome, quantidade, valor_total):
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font("Times", "B", 24)
+    pdf.cell(0, 15, "FAZENDA SERTÃO", ln=True, align="C")
+
+    pdf.set_font("Times", "", 12)
+    pdf.cell(0, 8, "Nota Fiscal", ln=True, align="C")
+
+    pdf.ln(5)
+
+    pdf.line(10, pdf.get_y(), 200, pdf.get_y())
+    pdf.ln(5)
+
+    pdf.set_font("Times", "", 12)
+    pdf.cell(32, 8, "Data de Emissão:")
+    pdf.set_font("Times", "B", 12)
+    pdf.cell(0, 8, datetime.now().strftime("%d/%m/%Y"), ln=True)
+
+    pdf.ln(5)
+
+    pdf.set_font("Times", "B", 14)
+    pdf.cell(0, 10, "DADOS DO CLIENTE", ln=True)
+
+    pdf.set_font("Times", "", 12)
+    pdf.cell(0, 8, f"Cliente: {cliente}", ln=True)
+    pdf.cell(0, 8, f"CEP: {endereco.get('cep')}", ln=True)
+    pdf.cell(0, 8, f"Endereco: {endereco.get('logradouro')}", ln=True)
+    pdf.cell(0, 8, f"Bairro: {endereco.get('bairro')}", ln=True)
+    pdf.cell(0, 8, f"Cidade: {endereco.get('localidade')} - {endereco.get('uf')}", ln=True)
+
+    pdf.ln(10)
+
+    pdf.set_font("Times", "B", 12)
+
+    pdf.cell(100, 10, "Produto", border=1)
+    pdf.cell(40, 10, "Quantidade", border=1)
+    pdf.cell(50, 10, "Valor", border=1, ln=True)
+
+    pdf.set_font("Times", "", 12)
+
+    pdf.cell(100, 10, f"{nome}", border=1)
+    pdf.cell(40, 10, f"x{quantidade}", border=1)
+    pdf.cell(50, 10, f"R$ {valor_total:.2f}", border=1, ln=True)
+
+    pdf.ln(10)
+
+    pdf.set_font("Times", "B", 14)
+    pdf.cell(0, 10, f"VALOR TOTAL: R$ {valor_total:.2f}", ln=True)
+
+    pdf.ln(20)
+
+    pdf.cell(0, 10, "_" * 35, ln=True, align="C")
+    pdf.cell(0, 8, "Assinatura do Responsável", align="C")
+
+    pdf.output("notas/nota_fiscal.pdf")
