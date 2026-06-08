0681dc566ba83720fd63682868d2429a58535c47
hazzard083 <valdeniogomesdesajunior@gmail.com>
2026-06-08 10:29:16 -0300

    ✨ feat(helpers): adiciona função para buscar endereço por CEP

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,18 @@
 import os
+import requests
 
 def clearCMD():
   os.system('cls' if os.name == 'nt' else 'clear')
+
+def buscar_endereco_por_cep(cep_destino):
+    response = requests.get(
+        f"https://viacep.com.br/ws/{cep_destino}/json/"
+    )
+
+    if response.status_code == 200:
+        return response.json()
+
+    return None
 
 def gerar_id(lista_tipos, lista, tipo):
 
